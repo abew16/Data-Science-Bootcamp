97f5c5efd7b220f4b44e2b85b817093f960d63e4
Abe Waziri <abe.waziri@gmail.com>
2017-09-11 21:51:10 -0700

    Crawler First Draft

--- Unit 3/Capstone/Crawler.py
+++ Unit 3/Capstone/Crawler.py
@@ -4,6 +4,7 @@ import requests
 BASE_URL = 'https://api.themoviedb.org/3/'
 API_KEY_3 = '934559315e2d3148d261fe2f126755db'
 API_KEY_4 = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MzQ1NTkzMTVlMmQzMTQ4ZDI2MWZlMmYxMjY3NTVkYiIsInN1YiI6IjU5OTEwYmQ4YzNhMzY4MDZjMzAwMjQ2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wFXnI4QLQckkcpYad3si4_VCqsd-ztu3pUMdrq0-K1M'
+payload = {'api_key' : API_KEY_3}
 
 """
 For movies, credits, and people
@@ -19,35 +20,50 @@ For movies, credits, and people
 
 seen_movies = set()
 seen_people = set()
-seen_credits = set()
-unseen_people_id = []
-unseen_movie_id = []
+unseen_people_ids = set()
+unseen_movie_ids = {819}
 
 db = sql.connect(':memory:')
 
 
 def create_tables():
-    db.execute('CREATE TABLE movies(movie_id integer,'
+    db.execute('CREATE TABLE IF NOT EXISTS movies(movie_id integer,'
                '                    budget integer,'
                '                    release_date text, '
-               '                    production_companies text,'
                '                    revenue integer,'
                '                    runtime integer,'
                '                    title text)')
-    db.execute('CREATE TABLE genres(movie_id text,'
+    db.execute('CREATE TABLE IF NOT EXISTS genres(movie_id text,'
                '                    genre_name text)')
-    db.execute('CREATE TABLE production_companies(movie_id text,'
+    db.execute('CREATE TABLE IF NOT EXISTS production_companies(movie_id text,'
                '                                  company_name text)')
-    db.execute('CREATE TABLE people(movie_id integer,'
+    db.execute('CREATE TABLE IF NOT EXISTS cast(movie_id integer,'
                '                    name text,'
                '                    gender integer,'
-               '                    order integer)')
+               '                    cast_order integer)')
+    db.execute('CREATE TABLE IF NOT EXISTS crew(movie_id integer,'
+               '                    name text,'
+               '                    gender integer,'
+               '                    job text)')
 
 
-def insert_people_data(json):
-    list_of_keys = ['']  # Add keys that you think are important for analysis
-    row = [json.get(key) for key in list_of_keys]
-    db.execute('INSERT INTO people VALUES (?, ?, ?, ?)', row)
+def insert_cast_data(json, movie_id):
+    list_of_keys = ['name', 'gender','order']  # Add keys that you think are important for analysis
+    cast = json.get('cast')
+    for cast_member in cast:
+        row = [cast_member.get(key) for key in list_of_keys]
+        row.insert(0, movie_id)
+        db.execute('INSERT INTO cast VALUES (?, ?, ?, ?)', row)
+        unseen_people_ids.add(cast_member['id'])
+
+
+def insert_crew_data(json, movie_id):
+    list_of_keys = ['movie_id', 'name', 'gender','job']  # Add keys that you think are important for analysis
+    crew = json.get('crew')
+    for crew_member in crew:
+        row = [crew_member.get(key) for key in list_of_keys]
+        row.insert(0, movie_id)
+        db.execute('INSERT INTO crew VALUES (?, ?, ?, ?)', row)
 
 
 def insert_movie_data(json):
@@ -67,51 +83,46 @@ def insert_movie_data(json):
     db.execute('INSERT INTO genres VALUES (?, ?)', row)
 
 def insert_movie_ids(json):
-    list_of_keys = ['id']  # Add keys that you think are important for analysis
-    row = [json.get(key) for key in list_of_keys]
-    db.execute('INSERT INTO people VALUES (?, ?, ?)', row)
-
-
-def save_people_credits(json):
-    return
-
+    cast = json['cast']
+    for movie in cast:
+        unseen_movie_ids.add(movie['id'])
 
 def check_if_movie_exists(movie_id):
-    movie_id in seen_movies
-    return True
+    return movie_id in seen_movies
 
 
-def get_movie(movie_id):
+def get_movie_data(movie_id):
     if check_if_movie_exists(movie_id):
         return
 
     # Fetch movie data
-    resp = requests.get(BASE_URL + f'movie/{movie_id}')
+    resp = requests.get(BASE_URL + f'movie/{movie_id}', params=payload)
     json = resp.json()
 
     # Save data
     insert_movie_data(json)
 
-    resp = requests.get(BASE_URL + f'movie/{movie_id}/credits')
+    resp = requests.get(BASE_URL + f'movie/{movie_id}/credits', params=payload)
     json = resp.json()
 
-    # Save the cast of the movie
-    insert_people_data(json)
+    # Save the cast & crew of the movie
+    insert_cast_data(json, movie_id)
+    insert_crew_data(json, movie_id)
 
     # Mark as seen
     seen_movies.add(movie_id)
 
 
 def check_if_person_exists(person_id):
-    person_id in seen_people
-    return True
+    return person_id in seen_people
 
 
-def get_person(person_id):
+
+def get_movie_ids_from_person(person_id):
     if check_if_person_exists(person_id):
         return
 
-    resp = requests.get(BASE_URL + f'person/{person_id}/movie_credits')
+    resp = requests.get(BASE_URL + f'person/{person_id}/movie_credits', params=payload)
     json = resp.json()
 
     insert_movie_ids(json)
@@ -124,12 +135,19 @@ def get_person(person_id):
 # [P5 P9]
 # DB P1 P2 P3
 
-while len(db) < 10000:
-    while len(movie_list) > 0:
-        movie_id = movie_list.pop()
-        get_movie(movie_id)
+create_tables()
 
-    while len(person_list) > 0:
-        person_id = person_list.pop()
-        get_person(person_id)
 
+
+while len(seen_movies) < 2:
+    while len(unseen_movie_ids) > 0:
+        movie_id = unseen_movie_ids.pop()
+        get_movie_data(movie_id)
+
+    while len(unseen_people_ids) > 0:
+        person_id = unseen_people_ids.pop()
+        get_movie_ids_from_person(person_id)
+
+    db.commit()
+
+db.close()
